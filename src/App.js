e024cee1d6d6a6fd18ff9d3485786fa675609933
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-07 14:57:52 -0700

    Import all four button Components into file using import keyword

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
+
 
 function App() {
   return (
