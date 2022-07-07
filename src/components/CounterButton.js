c3644130b721b6250be899fc3825fc2987e1b7a1
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-07 14:43:27 -0700

    Write function with return statement containing starter HTML from Compass reading

---
+++ src/components/CounterButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CounterButton() {
+
+  return (
+    <button className="CounterButton">You clicked me X amount of times</button>
+  );
+};
+
+export default CounterButton;
