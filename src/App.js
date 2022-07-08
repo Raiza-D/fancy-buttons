7deb1db49dcbaf6bf3f111942b375171a1ce5eca
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-07 19:10:03 -0700

    Added logic changing state of LightSwitchButton from that file into here. Changed function name to switchLight. Pass in switchLight as a prop. Remove setLight as a prop on Line 22

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 import { useState } from 'react';
 
-
 function App() {
   const [light, setLight] = useState("off");
   const dark = (light === "off") ? "dark" : "";
+
+  const switchLight = () => {
+    setLight((light === "on") ? "off" : "on");
+  };
 
   return (
     <div className={`App ${dark}`}>
@@ -16,7 +19,7 @@ function App() {
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton light={light} setLight={setLight}/>
+        <LightSwitchButton light={light} switchLight={switchLight}/>
         <TextRepeaterButton />
       </section>
     </div>
